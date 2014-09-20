1f2ad4872c9f6a4ee9cd140f37efcd6956779a82
hans97 <hans.aribowo@gmail.com>
2014-09-20 23:04:49 +0800

    Separate event tasks, dataflow tasks, and files to separate tabs

--- dexen/server/frontend/app/executions.js
+++ dexen/server/frontend/app/executions.js
@@ -21,7 +21,6 @@ function refreshExecutionsTable() {
 		return;
 	}
 
-	var $table = $('#executionsTable');
 	var ajaxUrl = '/executions/' + jobName;
 	console.log('Loading executions table from ' + ajaxUrl);
 
@@ -30,30 +29,32 @@ function refreshExecutionsTable() {
         url: ajaxUrl,
         dataType: "json",
         success: function (data) {
-			if($.fn.dataTable.isDataTable($table) ) 		
-				$table.DataTable().destroy();
-
-			$table.dataTable({
-		    	"data": data.executions,
-		    	"columns": [
-					{ "title" : "ID"			,	"data": null   ,   "render" : executionsTableRenderFunction("execution_id") },
-					{ "title" : "Task Name" 	,	"data": null   ,   "render" : executionsTableRenderFunction("task_name") },
-					{ "title" : "WorkerName"	,	"data": null   ,   "render" : executionsTableRenderFunction("worker_name") },
-					{ "title" : "Creation Time"	,	"data": null   ,   "render" : executionsTableRenderFunction("creation_time") },
-					{ "title" : "Begin Time"	,	"data": null   ,   "render" : executionsTableRenderFunction("begin_time") },
-					{ "title" : "End Time"		,	"data": null   ,   "render" : executionsTableRenderFunction("end_time") },
-					{ "title" : "Status"		,	"data": null   ,   "render" : executionsTableRenderFunction("status") },
-					{ "title" : "Stdout"		,	"data": null   ,   "render" : executionsTableRenderFunction("stdout") },
-					{ "title" : "Stderr"		,	"data": null   ,   "render" : executionsTableRenderFunction("stderr") }
-				]
-		    });	    
-		
+			var table = $('#executionsTable').DataTable();
+			table
+				.clear()
+				.rows.add(data.executions)
+				.draw();
 		}
 	});
 }
 
 function setupExecutionsTable() {
-    var $refreshBtn = $('#refreshExecutionsTableBtn');
+	var $table = $('#executionsTable');
+	$table.dataTable({
+    	"data": [],
+    	"columns": [
+			{ "title" : "ID"			,	"data": null   ,   "render" : executionsTableRenderFunction("execution_id") },
+			{ "title" : "Task Name" 	,	"data": null   ,   "render" : executionsTableRenderFunction("task_name") },
+			{ "title" : "WorkerName"	,	"data": null   ,   "render" : executionsTableRenderFunction("worker_name") },
+			{ "title" : "Creation Time"	,	"data": null   ,   "render" : executionsTableRenderFunction("creation_time") },
+			{ "title" : "Begin Time"	,	"data": null   ,   "render" : executionsTableRenderFunction("begin_time") },
+			{ "title" : "End Time"		,	"data": null   ,   "render" : executionsTableRenderFunction("end_time") },
+			{ "title" : "Status"		,	"data": null   ,   "render" : executionsTableRenderFunction("status") },
+			{ "title" : "Stdout"		,	"data": null   ,   "render" : executionsTableRenderFunction("stdout") },
+			{ "title" : "Stderr"		,	"data": null   ,   "render" : executionsTableRenderFunction("stderr") }
+		]
+    });	   
 
+    var $refreshBtn = $('#refreshExecutionsTableBtn');
     $refreshBtn.on("click", refreshExecutionsTable);
 }
