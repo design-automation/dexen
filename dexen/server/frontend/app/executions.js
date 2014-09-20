a17ef101b72d32ae1eea091413a98b0f69861351
hans97 <hans.aribowo@gmail.com>
2014-09-20 17:31:49 +0800

    Changed selected row color and fixed execution table render problem

--- dexen/server/frontend/app/executions.js
+++ dexen/server/frontend/app/executions.js
@@ -1,3 +1,19 @@
+function executionsTableRenderFunction(key){
+	var localKey = key;
+	return function(data, type, row, meta){
+		if(!row.hasOwnProperty(localKey))
+			return "";
+
+		var cellData = row[localKey];
+
+		if(type == "display" && typeof(cellData) == "string"){
+			return escapeHtml(cellData);
+		}
+
+		return cellData;
+	};
+}
+
 function refreshExecutionsTable() {
 	var jobName = getCurrentJobNameFromTable();
 	if(!jobName){
@@ -8,27 +24,32 @@ function refreshExecutionsTable() {
 	var $table = $('#executionsTable');
 	var ajaxUrl = '/executions/' + jobName;
 	console.log('Loading executions table from ' + ajaxUrl);
-	if($.fn.dataTable.isDataTable($table) ) {		
-		$table.DataTable().ajax.url(ajaxUrl).load();
-	} else{
-		$table.dataTable({
-	    	"ajax": {		    
-			    "dataSrc": "executions",
-			    "url": ajaxUrl
-			},
-	        "columns": [
-				{ "title" : "ID"			,	"data": "execution_id" },
-				{ "title" : "Task Name" 	,	"data": "task_name" },
-				{ "title" : "WorkerName"	,	"data": "worker_name" },
-				{ "title" : "Creation Time"	,	"data": "creation_time" },
-				{ "title" : "Begin Time"	,	"data": "begin_time" },
-				{ "title" : "End Time"		,	"data": "end_time" },
-				{ "title" : "Status"		,	"data": "status" },
-				{ "title" : "Stdout"		,	"data": "stdout" },
-				{ "title" : "Stderr"		,	"data": "stderr" }
-			]
-	    });	    
-	}
+
+	$.ajax({
+        type: "GET",
+        url: ajaxUrl,
+        dataType: "json",
+        success: function (data) {
+			if($.fn.dataTable.isDataTable($table) ) 		
+				$table.DataTable().destroy();
+
+			$table.dataTable({
+		    	"data": data.executions,
+		    	"columns": [
+					{ "title" : "ID"			,	"data": null   ,   "render" : executionsTableRenderFunction("execution_id") },
+					{ "title" : "Task Name" 	,	"data": null   ,   "render" : executionsTableRenderFunction("task_name") },
+					{ "title" : "WorkerName"	,	"data": null   ,   "render" : executionsTableRenderFunction("worker_name") },
+					{ "title" : "Creation Time"	,	"data": null   ,   "render" : executionsTableRenderFunction("creation_time") },
+					{ "title" : "Begin Time"	,	"data": null   ,   "render" : executionsTableRenderFunction("begin_time") },
+					{ "title" : "End Time"		,	"data": null   ,   "render" : executionsTableRenderFunction("end_time") },
+					{ "title" : "Status"		,	"data": null   ,   "render" : executionsTableRenderFunction("status") },
+					{ "title" : "Stdout"		,	"data": null   ,   "render" : executionsTableRenderFunction("stdout") },
+					{ "title" : "Stderr"		,	"data": null   ,   "render" : executionsTableRenderFunction("stderr") }
+				]
+		    });	    
+		
+		}
+	});
 }
 
 function setupExecutionsTable() {
